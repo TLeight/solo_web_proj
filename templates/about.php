919f75166bcccc34142fd8404e7a79625209063c
Tyler Leight <fireraider98@gmail.com>
2024-06-30 14:20:23 -0400

    trying to fix issue

--- templates/about.php
+++ templates/about.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Me - My Coding Skills</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <header>
@@ -12,7 +12,7 @@
         <nav>
             <ul>
                 <li><a href="#skills">Skills</a></li>
-                <li><a href="templates/about.php">About Me</a></li>
+                <li><a href="about.php">About Me</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
@@ -27,12 +27,12 @@
             programming languages and technologies.</p>
         <p>In my spare time, I enjoy exploring new technologies, contributing to open-source projects, and participating 
             in coding challenges. I believe in continuous learning and strive to stay updated with the latest industry trends.</p>
-        <p>If you'd like to learn more about my work or get in touch, feel free to visit the <a href="index.php#contact">Contact</a> 
+        <p>If you'd like to learn more about my work or get in touch, feel free to visit the <a href="../index.php#contact">Contact</a> 
             section.</p>
     </section>
     <footer>
         <p>&copy; 2024 Tyler Leight. All rights reserved.</p>
     </footer>
-    <script src="scripts.js"></script>
+    <script src="../scripts.js"></script>
 </body>
 </html>
